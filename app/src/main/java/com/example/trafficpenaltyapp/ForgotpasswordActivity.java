be7369fffba11020f6acc4a776039184fad61e65
himani1026 <trafficpenalty.app10@gmail.com>
2019-12-11 12:17:20 +0530

    forgot psw, reset psw, verification psw page

--- app/src/main/java/com/example/trafficpenaltyapp/ForgotpasswordActivity.java
+++ app/src/main/java/com/example/trafficpenaltyapp/ForgotpasswordActivity.java
@@ -5,24 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class ForgotpasswordActivity extends AppCompatActivity {
 
-    TextView txt_forgotpassword;
+    Button btn_submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_forgotpassword);
 
-        txt_forgotpassword=(TextView) findViewById(R.id.txt_forgotpassword);
+        btn_submit=(Button) findViewById(R.id.btn_submit);
 
-        txt_forgotpassword.setOnClickListener(new View.OnClickListener() {
+        btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this,ForgotpasswordActivity.class);
+                Intent i = new Intent(ForgotpasswordActivity.this,VerificationActivity.class);
                 startActivity(i);
             }
         });
