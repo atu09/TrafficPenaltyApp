33a95c85cdb92f14c6e9194b6aa55a9bc58b76d4
himani1026 <trafficpenalty.app10@gmail.com>
2019-12-30 12:11:33 +0530

    police login and verification pages, user home page

---
+++ app/src/main/java/com/example/trafficpenaltyapp/PolicehomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.trafficpenaltyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PolicehomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_policehome);
+    }
+}
---
+++ app/src/main/java/com/example/trafficpenaltyapp/policelogin.java
@@ -0,0 +1,62 @@
+package com.example.trafficpenaltyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.trafficpenaltyapp.utils.CommonFunction;
+import com.example.trafficpenaltyapp.utils.Constants;
+import com.example.trafficpenaltyapp.utils.DataInterface;
+import com.example.trafficpenaltyapp.utils.Webservice_Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class policelogin extends AppCompatActivity implements DataInterface {
+
+    Button btn_login;
+    EditText edt_data;
+    Webservice_Volley volley;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_policelogin);
+
+        btn_login=(Button) findViewById(R.id.btn_login);
+        edt_data=(EditText) findViewById(R.id.edt_data);
+
+        volley=new Webservice_Volley(this,this);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!CommonFunction.checkMobilenumber(edt_data.getText().toString()))
+                {
+                    edt_data.setError("Enter 10 digit Mobile Number");
+                    return;
+                }
+
+
+                String url = Constants.Webserive_Url+"policedetails.php";
+
+                HashMap<String,String> params = new HashMap<>();
+                params.put("mobile number",edt_data.getText().toString());
+
+
+                volley.CallVolley(url,params,"login");
+
+            }
+        });
+    }
+
+    @Override
+    public void getData(JSONObject jsonObject, String tag) {
+
+    }
+}
