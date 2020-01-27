18cf7c1f36e3c7284906743c52ea63ac6bae62f5
himani1026 <trafficpenalty.app10@gmail.com>
2020-01-27 12:34:12 +0530

    validation in generate penalty activity

--- app/src/main/java/com/example/trafficpenaltyapp/GeneratePenaltyActivity.java
+++ app/src/main/java/com/example/trafficpenaltyapp/GeneratePenaltyActivity.java
@@ -3,13 +3,16 @@ package com.example.trafficpenaltyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.trafficpenaltyapp.models.PenaltyInfoVo;
 import com.example.trafficpenaltyapp.utils.CommonFunction;
@@ -20,6 +23,8 @@ import com.google.gson.Gson;
 
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
 public class GeneratePenaltyActivity extends AppCompatActivity implements DataInterface {
@@ -27,11 +32,14 @@ public class GeneratePenaltyActivity extends AppCompatActivity implements DataIn
     EditText edt_penaltydate;
     EditText edt_penaltytime;
     EditText edt_amount;
+    EditText edt_regno;
 
     Button btn_submit;
     LinearLayout ll_reasons;
 
     Webservice_Volley volley;
+
+    double total = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +49,7 @@ public class GeneratePenaltyActivity extends AppCompatActivity implements DataIn
         edt_penaltydate=(EditText) findViewById(R.id.edt_penaltydate);
         edt_penaltytime=(EditText) findViewById(R.id.edt_penaltytime);
         edt_amount=(EditText) findViewById(R.id.edt_amount);
+        edt_regno=(EditText) findViewById(R.id.edt_regno);
         btn_submit=(Button) findViewById(R.id.btn_submit);
         ll_reasons=(LinearLayout) findViewById(R.id.ll_reasons);
 
@@ -53,7 +62,7 @@ public class GeneratePenaltyActivity extends AppCompatActivity implements DataIn
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!CommonFunction.checkString(edt_penaltydate.getText().toString()))
+                /*if (!CommonFunction.checkString(edt_penaltydate.getText().toString()))
                 {
                     edt_penaltydate.setError("Enter valid date");
                     return;
@@ -69,18 +78,42 @@ public class GeneratePenaltyActivity extends AppCompatActivity implements DataIn
                 {
                     edt_amount.setError("Enter Valid Amount");
                     return;
+                }*/
+
+                if (!CommonFunction.checkString(edt_regno.getText().toString()))
+                {
+                    edt_regno.setError("Enter Vehicle Registration Details.");
+                    return;
                 }
 
-                String url = Constants.Webserive_Url+"login.php";
+                StringBuilder sb = new StringBuilder();
+
+                if (ll_reasons.getChildCount() > 0) {
+                    for (int i =0;i<ll_reasons.getChildCount();i++) {
+                        CheckBox chk = (CheckBox) ll_reasons.getChildAt(i);
+                        if (chk.isChecked()) {
+                            sb.append(chk.getText().toString()).append(",");
+                        }
+                    }
+                }
+
+
+
+                String url = Constants.Webserive_Url+"generate_penalty.php";
 
                 HashMap<String,String> params = new HashMap<>();
-                params.put("penalty date",edt_penaltydate.getText().toString());
-                params.put("penalty time",edt_penaltytime.getText().toString());
-                params.put("amount",edt_amount.getText().toString());
-                params.put("penalty_reason","");
-                params.put("penalty_status","");
-                params.put("rcbook_id","");
+                params.put("penalty_date",new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+                params.put("penalty_time",new SimpleDateFormat("HH:mm").format(new Date()));
+                params.put("amount","" + total);
+                params.put("penalty_reason",sb.toString());
+                params.put("penalty_status","pending");
+                params.put("reg_number",edt_regno.getText().toString());
+                params.put("rcbook_id",edt_regno.getText().toString());
                 params.put("iv_id","");
+
+                Log.d("REQ",params.toString());
+
+                volley.CallVolley(url,params,"generate_penalty");
 
 
             }
@@ -108,8 +141,21 @@ public class GeneratePenaltyActivity extends AppCompatActivity implements DataIn
 
                             View vs = LayoutInflater.from(this).inflate(R.layout.layout_checkbox,null);
 
-                            CheckBox chk = (CheckBox)vs.findViewById(R.id.checkBox);
+                            final CheckBox chk = (CheckBox)vs.findViewById(R.id.checkBox);
                             chk.setText(penaltyInfoVo.getResult().get(i).getReasonDetails() + "(Rs. " + penaltyInfoVo.getResult().get(i).getAmount() +")");
+                            chk.setTag(penaltyInfoVo.getResult().get(i).getAmount());
+
+                            chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                                @Override
+                                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                                    if (isChecked) {
+                                        total = total + Double.parseDouble(chk.getTag().toString());
+                                    }
+                                    else {
+                                        total = total - Double.parseDouble(chk.getTag().toString());
+                                    }
+                                }
+                            });
 
                             ll_reasons.addView(vs);
 
@@ -126,6 +172,9 @@ public class GeneratePenaltyActivity extends AppCompatActivity implements DataIn
             }
 
 
+        }
+        else {
+            Toast.makeText(this, jsonObject.toString(), Toast.LENGTH_SHORT).show();
         }
 
     }
--- app/src/main/java/com/example/trafficpenaltyapp/utils/Constants.java
+++ app/src/main/java/com/example/trafficpenaltyapp/utils/Constants.java
@@ -6,7 +6,7 @@ package com.example.trafficpenaltyapp.utils;
 public class Constants {
 
 
-   public static final String Webserive_Url="http://192.168.2.5/project_tpa/api/";
+   public static final String Webserive_Url="http://192.168.2.9/project_tpa/api/";
 
 
 //   public static final String IMAGE_Url="http://192.168.1.3/Nutrition_Diet_App/Uploads/";
